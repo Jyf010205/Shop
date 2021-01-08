502fa525be763ffe8eaed2e3a1b32b2c11ff538d
Jyf010205 <785923055@qq.com>
2021-01-08 16:52:48 +0800

    2021年1月8日16:52:41

---
+++ shop-auth/src/main/java/com/shuaibi/shop/ShopAuthApplication.java
@@ -0,0 +1,16 @@
+package com.shuaibi.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: jianyufeng
+ * @date: 2021/1/8 15:50
+ * @description:
+ */
+@SpringBootApplication
+public class ShopAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAuthApplication.class, args);
+    }
+}
