b630c9a7134c417626b911355fb237f467073169
Jyf010205 <785923055@qq.com>
2021-01-13 15:44:00 +0800

    添加公共类

---
+++ shop-common/src/main/java/com/shuaibi/shop/common/entity/result/IErrorCode.java
@@ -0,0 +1,12 @@
+package com.shuaibi.shop.common.entity.result;
+
+/**
+ * @author: jianyufeng
+ * @date: 2021/1/13 15:38
+ * @description:
+ */
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
